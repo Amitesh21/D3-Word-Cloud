531bbbc6bddfd078ac3aed9781793b12b3fa4cc6
Amitesh Jaiswal <amitesh.jaiswal21@gmail.com>
2016-08-06 04:58:33 -0700

    edit read file func and remove extra files

--- README.md
+++ README.md
@@ -1,21 +1,15 @@
-# Word Cloud
-
-1. Developed a Word Cloud based using D3.js. 
-2. Reading the files from a directory of type pdf or docx.
-3. Generating a HashMap with each word and its count in all the documents. 
-4. Using this metrics to decide the size of an indvidual word and generating the Word-Cloud.
-
 Sample WordCloud:
 
 <img width="763" alt="screen shot 2016-07-15 at 12 20 35 am" src="https://cloud.githubusercontent.com/assets/14050490/16867122/e769cc68-4a24-11e6-8e01-4aca639e14e4.png">
 
-## Developing
+
+# Word Cloud
+
+1. Developed a Word Cloud of Doctor's Prescriptions using D3.js. 
+2. Reading the files from entire directory and extracting all the words.
+3. Filtering the common and unimportant words using arraylist.
+4. Generating a HashMap with each word and its count in all the prescriptions. 
+5. Evaluating the importance based on particular metrics to find the size of an indvidual word.
 
 
-
-### Tools
-
-Created with [Nodeclipse](https://github.com/Nodeclipse/nodeclipse-1)
- ([Eclipse Marketplace](http://marketplace.eclipse.org/content/nodeclipse), [site](http://www.nodeclipse.org))   
-
-Nodeclipse is free open-source project that grows with your contributions.
+Technology Used: Express, Node File System module, D3.js.
--- routes/readDirectory.js
+++ routes/readDirectory.js
@@ -1,10 +1,13 @@
 //Import file system functionality
 var MapArr = [];
-exports.readDirectory = function(req,res) {
+var fs = require('fs');
 
-	var fs = require('fs');
-	var folderName= '/Users/amiteshjaiswal/Documents/Angular Widgets/D3-Word-Cloud/files/';
-	console.log("folder: "+folderName);
+exports.readDirectory = function(req,res) {
+	var fileCount =0;
+	// creating a list of unnecessary keywords
+	var excludeChar = ['is','and','the','have','that','will','but','any','for','to','of','has','a','with','by','are','as','per','some','of',
+						'can','we','than','or','in','an','all','who','what','on','our','too','know','much','given','cannot','which','some'];
+	var folderName= './prescriptions';
 	if (folderName == undefined) {
 		console.log("Please specify a folder");
 		return;
@@ -16,39 +19,51 @@ exports.readDirectory = function(req,res) {
 		if (err) {
 			throw err; 
 		}
-		
-		
-		//console.log("length---------------------: "+data.length);
+
+		for(var i=0; i<data.length; i++){
+			if(data[i].indexOf('.txt')!==-1 || data[i].indexOf('.pdf')!==-1)
+				fileCount++;
+		}
+
 		data.forEach(function(file){ 
 			fs.readFile(folderName + "/" + file, 'utf8', function(err, filedata){ //read each file in array
 				if (err) {
 				throw err;
 				} 
-				console.log("file: "+file);
+				// checking file extension
 				if(file.indexOf('.txt')!==-1 || file.indexOf('.pdf')!==-1){
+					fileCount--;
 					var testingVariable = filedata.split(" ");
 					
 					for(var j=0; j<testingVariable.length;j++){
-						console.log(testingVariable[j]);
+
 						var flag = 0;
+						// if the keyword is in exclude list
+						for(var k=0; k<excludeChar.length; k++){
+							if(testingVariable[j] == excludeChar[k])
+								flag=1;
+						}
+						// if already exist in mapArr, increase its count
 						for(var i=0;i<MapArr.length;i++){
 							if(MapArr[i].text === testingVariable[j]){
 								flag =1;
 								MapArr[i].size = MapArr[i].size+1;
 							}
 						}
+						// if not exist, add it in mapArr
 						if(flag === 0)
 						{
 							MapArr.push({text: testingVariable[j], size: 1});
 						}
 					}
-					console.log(MapArr);
-					fs.writeFileSync("./public/d3WordCloud/scrape_data.js","var scrapeData = "+JSON.stringify(MapArr));
-					res.render('wordCloud');
+					// will write mapArr when all entire directory files are read
+					if(fileCount==0){
+						fs.writeFile("./public/d3WordCloud/scrape_data.js","var scrapeData = "+JSON.stringify(MapArr));
+						console.log("readDirectory end");
+						res.render('wordCloud');
+					}
 				}
 			});
-			
 		});
-		
 	});
 };
